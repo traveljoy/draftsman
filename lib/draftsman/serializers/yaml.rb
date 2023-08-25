79912cc3ba873d79b2f7fc6b0df00648c11a3c40
Tommy Crumrine <tommy@traveljoy.com>
2023-08-24 20:42:01 -0500

    use 3.1+ syntax if version at least 3.1

--- lib/draftsman/serializers/yaml.rb
+++ lib/draftsman/serializers/yaml.rb
@@ -6,11 +6,21 @@ module Draftsman
       extend self # makes all instance methods become module methods as well
 
       def load(string)
+        if at_least_ruby_3_1?
+          YAML.load string, permitted_classes: [Date, Time, BigDecimal], aliases: true
+        else
+          YAML.load string
+        end
         YAML.load string
       end
 
       def dump(object)
         YAML.dump object
+      end
+
+      def at_least_ruby_3_1?
+        major, minor, _patch = RUBY_VERSION.split(".").map(&:to_i)
+        (major >= 3 && minor >= 1) || major > 3
       end
     end
   end
